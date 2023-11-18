22cb6e9de5cdd9b6f88609b661cf7b4a9d52fefd
natepetersen03 <chepic123@gmail.com>
2023-11-18 04:49:57 -0500

    Added unique ids for every user. Will push this to MongoDB and create tables.

---
+++ fraudulert-backend/addId.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("credit_card_fraud.csv")
+dict = {}
+id = 1
+for index, line in df.iterrows():
+    curr = (line["job"], line["dob"])
+    if not (curr in dict.keys()):
+        dict[curr] = id
+        id += 1
+    df.at[index, "id"] = dict[curr]
+df.to_csv("updated_credit.csv")
+    
